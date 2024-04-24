71ab27abf2c2501b50b3ab605334afc611ee0b4f
VeronikaTsitser <kondratukveronika@gmail.com>
2024-04-24 20:10:14 +0100

    Sort foodConsumingList by time in FoodConsumingRepositoryImpl

--- lib/features/calorie_counter/data/food_consuming_repository_impl.dart
+++ lib/features/calorie_counter/data/food_consuming_repository_impl.dart
@@ -9,14 +9,22 @@ class FoodConsumingRepositoryImpl implements FoodConsumingRepository {
   @override
   Future<List<FoodConsumingModel>> getFoodConsuming() async {
     final pref = await SharedPreferences.getInstance();
+
     final foodConsumingList = pref.getStringList('foodConsumingList') ?? [];
+
     log('foodConsumingList: $foodConsumingList');
-    return foodConsumingList.map((e) => FoodConsumingModel.fromJson(jsonDecode(e))).toList();
+    List<FoodConsumingModel> sortedList = [];
+    foodConsumingList.map((e) {
+      sortedList.add(FoodConsumingModel.fromJson(jsonDecode(e)));
+      return sortedList;
+    }).toList();
+    return sortedList;
   }
 
   @override
   Future<void> setFoodConsuming(List<FoodConsumingModel> foodConsumingList) async {
     final pref = await SharedPreferences.getInstance();
+    foodConsumingList.sort((a, b) => a.time.compareTo(b.time));
     await pref.setStringList('foodConsumingList', foodConsumingList.map((e) => jsonEncode(e)).toList());
   }
 }
